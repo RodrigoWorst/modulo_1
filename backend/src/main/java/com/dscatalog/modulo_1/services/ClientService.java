839676647e20eb42a282bbda0ffb60389fb88450
RodrigoWorst <rodrigobworst@live.com>
2022-07-30 10:18:06 -0300

    EndPoint GET (findAllPaged) and insert rows into tb_client

---
+++ backend/src/main/java/com/dscatalog/modulo_1/services/ClientService.java
@@ -0,0 +1,25 @@
+package com.dscatalog.modulo_1.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.dscatalog.modulo_1.dto.ClientDTO;
+import com.dscatalog.modulo_1.entities.Client;
+import com.dscatalog.modulo_1.repositories.ClientRepository;
+
+@Service
+public class ClientService {
+	
+	@Autowired
+	private ClientRepository repository;
+	
+	@Transactional(readOnly = true)
+	public Page<ClientDTO> findAllPaged(PageRequest pageRequest){
+		Page<Client> list = repository.findAll(pageRequest);
+		return list.map(x -> new ClientDTO(x));
+	}
+
+}
