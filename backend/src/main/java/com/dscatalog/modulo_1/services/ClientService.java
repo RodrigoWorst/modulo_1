8c1693dcfb8120a0fb2579478a0900c2b339817c
RodrigoWorst <rodrigobworst@live.com>
2022-07-30 10:43:43 -0300

    EndPoint GET (findById) and exception ResourceNotFound

--- backend/src/main/java/com/dscatalog/modulo_1/services/ClientService.java
+++ backend/src/main/java/com/dscatalog/modulo_1/services/ClientService.java
@@ -1,5 +1,7 @@
 package com.dscatalog.modulo_1.services;
 
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
@@ -9,6 +11,7 @@ import org.springframework.transaction.annotation.Transactional;
 import com.dscatalog.modulo_1.dto.ClientDTO;
 import com.dscatalog.modulo_1.entities.Client;
 import com.dscatalog.modulo_1.repositories.ClientRepository;
+import com.dscatalog.modulo_1.services.exceptions.ResourceNotFoundException;
 
 @Service
 public class ClientService {
@@ -20,6 +23,12 @@ public class ClientService {
 	public Page<ClientDTO> findAllPaged(PageRequest pageRequest){
 		Page<Client> list = repository.findAll(pageRequest);
 		return list.map(x -> new ClientDTO(x));
+	}
+
+	public ClientDTO findById(Long id) {
+		Optional<Client> obj = repository.findById(id);
+		Client entity = obj.orElseThrow(()-> new ResourceNotFoundException("Entity not found!"));
+		return new ClientDTO(entity);
 	}
 
 }
